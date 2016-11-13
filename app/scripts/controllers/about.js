18eae6d02523d44c342d27779a808343162f81c3
gudlarakesh <gudlarakesh1991@gmail.com>
2016-11-13 21:40:03 +0530

    initial commit

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,13 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name abckidsworldApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the abckidsworldApp
+ */
+angular.module('abckidsworldApp')
+  .controller('AboutCtrl', function () {
+    
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,13 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name abckidsworldApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the abckidsworldApp
+ */
+angular.module('abckidsworldApp')
+  .controller('MainCtrl', function () {
+    
+  });
