7c50d23cdecd168e80fa466c980a17f44bd0770d
gudlarakesh <gudlarakesh1991@gmail.com>
2017-03-15 14:26:02 +0530

    working on list in view product view

--- app/scripts/controllers/uploadproducts.js
+++ app/scripts/controllers/uploadproducts.js
@@ -11,6 +11,11 @@ angular.module('abckidsworldApp')
   .controller('UploadproductsCtrl', function ($scope, $rootScope, $routeParams, $location, Upload, cloudinary, productService) {
     $scope.categories = productService.getCategories();
     $scope.checkImage = true;
+    var productImage1 =  '' ;
+    var productImage2 = '' ;
+    var productImage3 = '' ;
+    var productImage4 = '' ;
+    var productImage5 = '' ;
 
     $scope.uploadProduct = function(files){
       $scope.files = files;
@@ -34,6 +39,40 @@ angular.module('abckidsworldApp')
       });
     };
 
+    function uploadProductImage(files,val){
+      $scope.files = files;
+      if (!$scope.files) {return}
+      angular.forEach(files, function(file){
+        if (file && !file.$error) {
+          file.upload = Upload.upload({
+            url: "https://api.cloudinary.com/v1_1/" + cloudinary.config().cloud_name + "/upload",
+            data: {
+              upload_preset: cloudinary.config().upload_preset,
+              file: file
+            }
+          }).success(function (data, status, headers, config) {
+            val = data.url;
+          }).error(function (data, status, headers, config) {
+            file.result = data;
+          });
+        }
+      });
+    }
+
+    $scope.uploadOtherProducts = function(file,num){
+      if(num === 1){
+        uploadProductImage(file, productImage1)
+      } else if (num === 2){
+        uploadProductImage(file, productImage2)
+      } else if(num === 3){
+        uploadProductImage(file, productImage3)
+      } else if(num === 4){
+        uploadProductImage(file, productImage4)
+      } else {
+        uploadProductImage(file, productImage5)
+      }
+    }
+
     $scope.details = {
       name: '',
       description: '',
@@ -46,7 +85,12 @@ angular.module('abckidsworldApp')
       topProductImage:'',
       newProductImage:'',
       topProduct: 'false',
-      newProduct: 'false'
+      newProduct: 'false',
+      productImage1: productImage1,
+      productImage2: productImage2,
+      productImage3: productImage3,
+      productImage4: productImage4,
+      productImage5: productImage5
     };
 
     $scope.uploadTopProduct = function(files){
@@ -115,7 +159,12 @@ angular.module('abckidsworldApp')
           topProductImage:'',
           newProductImage:'',
           topProduct: 'false',
-          newProduct: 'false'
+          newProduct: 'false',
+          productImage1: '',
+          productImage2: '',
+          productImage3: '',
+          productImage4: '',
+          productImage5: ''
         };
         $scope.checkImage = true;
         $scope.details = productService.product();
--- app/scripts/controllers/viewproduct.js
+++ app/scripts/controllers/viewproduct.js
@@ -21,11 +21,23 @@ angular.module('abckidsworldApp')
 
     var p = productService.getProductById($scope.productId).$loaded();
     p.then(function(val){
+      console.log(val);
       $scope.detail = val;
       var image = val.imageUrl;
       $scope.smallImage = image.substring(0,52)+ 'w_450,h_400/'+image.substring(52);
+      $scope.smallProductImage1 = $scope.detail.productImage1.substring(0,52)+ 'w_450,h_400/'+ $scope.detail.productImage1.substring(52);
+      $scope.smallProductImage2 = $scope.detail.productImage2.substring(0,52)+ 'w_450,h_400/'+ $scope.detail.productImage2.substring(52);
+      $scope.smallProductImage3 = $scope.detail.productImage3.substring(0,52)+ 'w_450,h_400/'+ $scope.detail.productImage3.substring(52);
+      $scope.smallProductImage4 = $scope.detail.productImage4.substring(0,52)+ 'w_450,h_400/'+ $scope.detail.productImage4.substring(52);
+      $scope.smallProductImage5 = $scope.detail.productImage5.substring(0,52)+ 'w_450,h_400/'+ $scope.detail.productImage5.substring(52);
     })
     
+    $scope.changeProductImage = function(data){
+      var changeImage = $scope.smallImage;
+      $scope.smallImage = data;
+      data = changeImage;
+    }
+
     $scope.buyProduct = function(id){
       productService.sendProductDetail(id);
       $location.url("contact");
